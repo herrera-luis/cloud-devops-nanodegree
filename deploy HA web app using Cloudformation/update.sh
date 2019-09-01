361be000b4c1fb142c2c04ef94d1210515ef7ab4
Luis Herrera <lherrera@thoughtworks.com>
2019-08-31 20:06:03 -0400

    fixs LoadBalancer subnet targets from private to public

--- deploy HA web app using Cloudformation/update.sh
+++ deploy HA web app using Cloudformation/update.sh
@@ -2,5 +2,5 @@ aws cloudformation update-stack \
 --stack-name $1 \
 --template-body file://$2 \
 --parameters file://$3 \
---region=us-east-1 \
+--region=us-west-1 \
 --capabilities CAPABILITY_NAMED_IAM
